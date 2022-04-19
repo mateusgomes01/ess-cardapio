01eee66d7e0fffedb5fa92009c110f906b27de80
Leticia Maciel <lcbm2@cin.ufpe.br>
2022-04-18 22:58:49 -0300

    Create Add item request on server endpoint

--- projeto/server/index.js
+++ projeto/server/index.js
@@ -12,6 +12,26 @@ const corsOptions ={
 
 app.use(cors(corsOptions));
 
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "cardapio"
+});
+
+//CREATE
+app.post('/add', (req, res) => {
+    const { name } = req.body;
+    const { price } = req.body;
+    const { description } = req.body;
+
+    let request = "INSERT INTO item (name, price, description) VALUES (?, ?, ?)";
+
+    db.query(request, [name, price, description], (err, result) => {
+        res.send(result);
+    });
+});
+
 app.listen(3001, () => {
     console.log("Server running!");
 });
