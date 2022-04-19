3fa422cd1b181cd8367df643a8dfde9f1df266a8
Leticia Maciel <lcbm2@cin.ufpe.br>
2022-04-18 23:26:25 -0300

    Test GET connection on Home

--- projeto/client/src/Home.js
+++ projeto/client/src/Home.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import {useNavigate} from "react-router-dom";
+import Axios from 'axios';
+
+import Add from "./Add";
 
 import Button from '@mui/material/Button';
 
 import logo from "./brigadeiro.png";
+
+import variables from './variables.json';
 
 const Home = () => {
 
     let navigate = useNavigate ();
 
     const [open, setOpen] = useState(false);
+    const [showItems, setShowItems] = useState();
+
+    useEffect(() => {
+        Axios.get(variables.URL + "all").then((response) => {
+          setShowItems(response.data);
+        }, [showItems]);
+    });
 
     return (
         <div className="container">
@@ -30,6 +42,15 @@ const Home = () => {
                 </div>
             
             </div>
+
+            <div className="MenuItems">
+                {typeof showItems !== "undefined" && showItems.map((item) => {
+                    return(
+                        <h2> I'm a test! </h2>
+                    );
+                })}
+            </div>
+
 
         </div>
     );
