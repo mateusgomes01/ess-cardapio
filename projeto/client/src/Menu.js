33e1ce5b3a35d0385e4a90d5d0a31adc7b7d39ad
Leticia Maciel <lcbm2@cin.ufpe.br>
2022-04-21 13:05:15 -0300

    Add Edit button on edit Item instance

--- projeto/client/src/Menu.js
+++ projeto/client/src/Menu.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import Item from './Item';
 
 const Menu = ({isMenu, showItems, setShowItems}) => {
@@ -9,13 +10,13 @@ const Menu = ({isMenu, showItems, setShowItems}) => {
             {typeof showItems !== "undefined" && showItems.map((item) => {
                 return(
                     <Item
-                        key={item.id} 
-                        showItems={showItems} 
-                        setShowItems={setShowItems}
+                        isMenu={isMenu}
                         id={item.id}
+                        description={item.description}
                         name={item.name}
                         price={item.price}
-                        description={item.description}
+                        showItems={showItems} 
+                        setShowItems={setShowItems}
                     />
                 );
             })}
